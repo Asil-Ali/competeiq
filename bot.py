e474bdd2284f867872ccb7db3a5eec3071f1887e
Asil-Ali <aliaaeel125@gmail.com>
2026-03-13 23:21:28 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -499,4 +499,9 @@ def main():
 
 
 if __name__ == "__main__":
+  import threading, http.server
+    def health():
+        server = http.server.HTTPServer(("0.0.0.0", 10000), http.server.BaseHTTPRequestHandler)
+        server.serve_forever()
+    threading.Thread(target=health, daemon=True).start()
     main()
